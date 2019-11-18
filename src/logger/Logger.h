a9157b5847fcab169e8f8528acd8befc88848ce2
yx-ren <yunxiang.r@gmail.com>
2019-11-18 18:27:55 +0800

    add logger class

---
+++ src/logger/Logger.h
@@ -0,0 +1,24 @@
+#ifndef __LOGGER_H__
+#define __LOGGER_H__
+
+enum LOG_LEVEL
+{
+    LOG_LEVEL_FAILED = 0,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_TRACE,
+    LOG_LEVEL_OFF
+};
+
+class Logger
+{
+public:
+
+priavte:
+
+};
+
+
+#endif
